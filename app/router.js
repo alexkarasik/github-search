cef246bc353bdcc10a923a39abaee69d1cd4a2a3
kinkorea <alexkarasik89@gmail.com>
2017-03-28 10:39:24 -0400

    Created routes for the favorites and repos pages with their links and models. The initial array I created in the routes was not displaying the contents I wanted it to and I am not sure why, but I'm going to move on.
    Next: Work on templates a bit more and get the model content to display

--- app/router.js
+++ app/router.js
@@ -10,6 +10,8 @@ Router.map(function () {
   this.route('sign-in');
   this.route('change-password');
   this.route('users');
+  this.route('favorites');
+  this.route('repos');
 });
 
 export default Router;
