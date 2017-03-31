18c72f749be1e7e629cd021a31798541d7560010
kinkorea <alexkarasik89@gmail.com>
2017-03-31 15:19:25 -0400

    Prepare to deploy.

--- config/environment.js
+++ config/environment.js
@@ -49,7 +49,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootUrl = 'https://github.com/alexkarasik/github-search';
+    ENV.rootUrl = '/github-search';
     ENV.locationType = 'hash';
     ENV.apiHost = 'https://alexkarasik.github.io/github-search/';
   }
